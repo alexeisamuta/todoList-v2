753b7e3ee3e9a69d740115c7de4705c93debe282
alexeisamuta <alexeisamuta@gmail.com>
2020-11-25 23:15:09 +0300

    Updates

---
+++ precache-manifest.024b5be5374dba79b879a5e0f8ef48de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2543b9728cd83d729722db1cc05d9263",
+    "url": "/todoList-v2/index.html"
+  },
+  {
+    "revision": "7b3d2fa807a412fb54bb",
+    "url": "/todoList-v2/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "10335fa8e16e252a7a9a",
+    "url": "/todoList-v2/static/js/2.6e6559c3.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todoList-v2/static/js/2.6e6559c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b3d2fa807a412fb54bb",
+    "url": "/todoList-v2/static/js/main.eb595366.chunk.js"
+  },
+  {
+    "revision": "205f5a11da7904376f89",
+    "url": "/todoList-v2/static/js/runtime-main.92d190cf.js"
+  }
+]);
